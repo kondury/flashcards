5b02109eb8cd04c2facc9890ae316f8ce21823b5
kondury <65617656+kondury@users.noreply.github.com>
2023-10-19 10:30:21 +0300

    minor fixes to member functions definition

--- cards-app-kafka/src/main/kotlin/ConsumerStrategyV1.kt
+++ cards-app-kafka/src/main/kotlin/ConsumerStrategyV1.kt
@@ -2,24 +2,20 @@ package com.github.kondury.flashcards.app.kafka
 
 import com.github.kondury.flashcards.cards.api.v1.apiV1Mapper
 import com.github.kondury.flashcards.cards.api.v1.models.IRequest
-import com.github.kondury.flashcards.cards.api.v1.models.IResponse
 import com.github.kondury.flashcards.cards.common.CardContext
 import com.github.kondury.flashcards.cards.mappers.v1.fromTransport
 import com.github.kondury.flashcards.cards.mappers.v1.toTransportCard
 
 class ConsumerStrategyV1 : ConsumerStrategy {
-    override fun topics(config: AppKafkaConfig): InputOutputTopics {
-        return InputOutputTopics(config.kafkaTopicInV1, config.kafkaTopicOutV1)
-    }
 
-    override fun serialize(source: CardContext): String {
-        val response: IResponse = source.toTransportCard()
-        return apiV1Mapper.writeValueAsString(response)
-    }
+    override fun topics(config: AppKafkaConfig) =
+        InputOutputTopics(config.kafkaTopicInV1, config.kafkaTopicOutV1)
 
-    override fun deserialize(value: String, target: CardContext) {
-        val request: IRequest = apiV1Mapper.readValue(value, IRequest::class.java)
+    override fun serialize(source: CardContext): String =
+        source.toTransportCard()
+            .let(apiV1Mapper::writeValueAsString)
+
+    override fun deserialize(value: String, target: CardContext) =
         apiV1Mapper.readValue(value, IRequest::class.java)
-        target.fromTransport(request)
-    }
+            .let(target::fromTransport)
 }
