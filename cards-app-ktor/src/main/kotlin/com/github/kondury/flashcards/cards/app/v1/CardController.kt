a941fba3cc9450d6edf30adce849dd0fca42c8db
kondury <65617656+kondury@users.noreply.github.com>
2023-10-19 23:15:19 +0300

    request processing unification - extracted processing into cards-app-common - unified ktor controller

--- cards-app-ktor/src/main/kotlin/com/github/kondury/flashcards/cards/app/v1/CardController.kt
+++ cards-app-ktor/src/main/kotlin/com/github/kondury/flashcards/cards/app/v1/CardController.kt
@@ -1,40 +1,25 @@
 package com.github.kondury.flashcards.cards.app.v1
 
-import com.github.kondury.flashcards.cards.api.v1.models.CardCreateRequest
-import com.github.kondury.flashcards.cards.api.v1.models.CardDeleteRequest
-import com.github.kondury.flashcards.cards.api.v1.models.CardReadRequest
+import com.github.kondury.flashcards.cards.api.v1.models.*
+import com.github.kondury.flashcards.cards.app.common.process
 import com.github.kondury.flashcards.cards.biz.FcCardProcessor
-import com.github.kondury.flashcards.cards.common.CardContext
-import com.github.kondury.flashcards.cards.mappers.v1.fromTransport
-import com.github.kondury.flashcards.cards.mappers.v1.toCardCreateResponse
-import com.github.kondury.flashcards.cards.mappers.v1.toCardDeleteResponse
-import com.github.kondury.flashcards.cards.mappers.v1.toCardReadResponse
+import com.github.kondury.flashcards.cards.mappers.v1.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 
+suspend fun ApplicationCall.createCard(processor: FcCardProcessor) = processV1(processor)
 
-suspend fun ApplicationCall.createCard(processor: FcCardProcessor) {
-    val request = receive<CardCreateRequest>()
-    val context = CardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toCardCreateResponse())
+suspend fun ApplicationCall.readCard(processor: FcCardProcessor) = processV1(processor)
+
+suspend fun ApplicationCall.deleteCard(processor: FcCardProcessor) = processV1(processor)
+
+internal suspend inline fun ApplicationCall.processV1(processor: FcCardProcessor) {
+    processor.process(
+        { it.fromTransport(receive<IRequest>()) },
+        { respond(it.toTransportCard()) }
+    )
 }
 
-suspend fun ApplicationCall.readCard(processor: FcCardProcessor) {
-    val request = receive<CardReadRequest>()
-    val context = CardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toCardReadResponse())
-}
 
-suspend fun ApplicationCall.deleteCard(processor: FcCardProcessor) {
-    val request = receive<CardDeleteRequest>()
-    val context = CardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toCardDeleteResponse())
-}
 
