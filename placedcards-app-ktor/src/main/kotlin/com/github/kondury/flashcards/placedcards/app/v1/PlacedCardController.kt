46122e94fa6b1fa0c946e9f6d3b2dc0dd2b103ae
kondury <65617656+kondury@users.noreply.github.com>
2023-10-23 17:46:13 +0300

    unified processing for placed cards ktor

--- placedcards-app-ktor/src/main/kotlin/com/github/kondury/flashcards/placedcards/app/v1/PlacedCardController.kt
+++ placedcards-app-ktor/src/main/kotlin/com/github/kondury/flashcards/placedcards/app/v1/PlacedCardController.kt
@@ -1,52 +1,30 @@
 package com.github.kondury.flashcards.placedcards.app.v1
 
-import com.github.kondury.flashcards.placedcards.api.v1.models.*
+import com.github.kondury.flashcards.placedcards.api.v1.models.IRequest
+import com.github.kondury.flashcards.placedcards.app.common.process
 import com.github.kondury.flashcards.placedcards.biz.FcPlacedCardProcessor
-import com.github.kondury.flashcards.placedcards.common.PlacedCardContext
-import com.github.kondury.flashcards.placedcards.mappers.v1.*
+import com.github.kondury.flashcards.placedcards.mappers.v1.fromTransport
+import com.github.kondury.flashcards.placedcards.mappers.v1.toTransportPlacedCard
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 
 
-suspend fun ApplicationCall.createPlacedCard(processor: FcPlacedCardProcessor) {
-    val request = receive<PlacedCardCreateRequest>()
-    val context = PlacedCardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toPlacedCardCreateResponse())
-}
+suspend fun ApplicationCall.createPlacedCard(processor: FcPlacedCardProcessor) = processV1(processor)
 
-suspend fun ApplicationCall.movePlacedCard(processor: FcPlacedCardProcessor) {
-    val request = receive<PlacedCardMoveRequest>()
-    val context = PlacedCardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toPlacedCardMoveResponse())
-}
+suspend fun ApplicationCall.movePlacedCard(processor: FcPlacedCardProcessor) = processV1(processor)
 
-suspend fun ApplicationCall.deletePlacedCard(processor: FcPlacedCardProcessor) {
-    val request = receive<PlacedCardDeleteRequest>()
-    val context = PlacedCardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toPlacedCardDeleteResponse())
-}
+suspend fun ApplicationCall.deletePlacedCard(processor: FcPlacedCardProcessor) = processV1(processor)
 
-suspend fun ApplicationCall.selectPlacedCard(processor: FcPlacedCardProcessor) {
-    val request = receive<PlacedCardSelectRequest>()
-    val context = PlacedCardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toPlacedCardSelectResponse())
-}
+suspend fun ApplicationCall.selectPlacedCard(processor: FcPlacedCardProcessor) = processV1(processor)
 
-suspend fun ApplicationCall.initPlacedCard(processor: FcPlacedCardProcessor) {
-    val request = receive<PlacedCardInitRequest>()
-    val context = PlacedCardContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toPlacedCardInitResponse())
+suspend fun ApplicationCall.initPlacedCard(processor: FcPlacedCardProcessor) = processV1(processor)
+
+internal suspend inline fun ApplicationCall.processV1(processor: FcPlacedCardProcessor) {
+    processor.process(
+        { it.fromTransport(receive<IRequest>()) },
+        { respond(it.toTransportPlacedCard()) }
+    )
 }
 
 
