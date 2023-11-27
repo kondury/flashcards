1b449a214ff9de9fecff8f2e3d1ab20d3d9f2fd1
kondury <65617656+kondury@users.noreply.github.com>
2023-11-27 17:29:30 +0300

    changed access modifier to internal

--- cards-biz/src/commonMain/kotlin/cards/biz/validation/afterValidation.kt
+++ cards-biz/src/commonMain/kotlin/cards/biz/validation/afterValidation.kt
@@ -6,7 +6,7 @@ import com.github.kondury.flashcards.cards.common.models.FcState
 import com.github.kondury.flashcards.cor.dsl.CorChainDsl
 import com.github.kondury.flashcards.cor.dsl.worker
 
-fun CorChainDsl<CardContext>.afterValidation(command: CardCommand) = worker {
+internal fun CorChainDsl<CardContext>.afterValidation(command: CardCommand) = worker {
     this.title = "Prepare context after successful validation of ${command.name} params"
     activeIf { state == FcState.RUNNING }
     handle {
