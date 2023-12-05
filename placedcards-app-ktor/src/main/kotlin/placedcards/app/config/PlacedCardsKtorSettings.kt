720900756e39d06d817ed8a10ed570ba5df5401d
kondury <65617656+kondury@users.noreply.github.com>
2023-12-05 13:42:11 +0300

    removed unnecessary comments

--- placedcards-app-ktor/src/main/kotlin/placedcards/app/config/PlacedCardsKtorSettings.kt
+++ placedcards-app-ktor/src/main/kotlin/placedcards/app/config/PlacedCardsKtorSettings.kt
@@ -2,7 +2,6 @@ package com.github.kondury.flashcards.placedcards.app.config
 
 import io.ktor.server.config.*
 
-// todo move common settings to flashcards-app-ktor
 data class PlacedCardsKtorSettings(
     val appUrls: List<String>,
     override val mode: String,
